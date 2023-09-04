97edf9c8e9fc92a2058ba2408c0e3e74358269d5
pawl1n <pinchuk.andryusha310212@gmail.com>
2023-09-04 15:33:38 +0300

    refactor: do what clippy says

--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,12 @@ fn main() {
     let mut input = String::new();
     let mut tree: Tree<i32> = Tree::new();
 
-    while let Ok(_) = std::io::stdin().read_line(&mut input) {
+    while std::io::stdin().read_line(&mut input).is_ok() {
         if input.is_empty() {
             break;
         }
 
-        let v: Vec<&str> = input.split(" ").collect();
+        let v: Vec<&str> = input.split(' ').collect();
         let n: i32 = v[1].trim().parse().unwrap();
 
         match v[0] {
