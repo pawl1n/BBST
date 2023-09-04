48b70076126e3c49fac5a17a27be3bd6c20bf537
pawl1n <pinchuk.andryusha310212@gmail.com>
2023-09-04 15:21:59 +0300

    refactor: remove redundant methods

--- src/tree.rs
+++ src/tree.rs
@@ -1,10 +1,9 @@
 mod node;
 
 use crate::tree::node::Node;
-use std::cmp::Ordering;
 use std::fmt::{Debug, Display, Formatter, Result};
 
-#[derive(Debug, Clone, Eq)]
+#[derive(Debug, Clone)]
 pub struct Tree<T: Ord> {
     pub root: Option<Box<Node<T>>>,
 }
@@ -47,23 +46,5 @@ impl<T: Ord + Default + Display + Debug> Tree<T> {
 impl<T: Display + Ord + Debug> Display for Tree<T> {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         write!(f, "{:?}", self.root)
-    }
-}
-
-impl<T: Ord> PartialOrd for Tree<T> {
-    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
-        Some(self.cmp(other))
-    }
-}
-
-impl<T: Ord> Ord for Tree<T> {
-    fn cmp(&self, other: &Self) -> Ordering {
-        self.root.cmp(&other.root)
-    }
-}
-
-impl<T: Ord> PartialEq for Tree<T> {
-    fn eq(&self, other: &Self) -> bool {
-        self.root == other.root
     }
 }
